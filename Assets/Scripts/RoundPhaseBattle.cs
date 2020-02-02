18e08b12b7ba114d088659895a76d3b857c54957
PeachTreeOath <peachtreeoath@gmail.com>
2020-02-01 21:43:51 -0700

    Getting rid of battle platform - KL

--- Assets/Scripts/RoundPhaseBattle.cs
+++ Assets/Scripts/RoundPhaseBattle.cs
@@ -7,7 +7,7 @@ public class RoundPhaseBattle : RoundPhase
 
     public float timeUntilPhaseEndAfterShrink;
 
-    public GameObject battlePhasePlatform;
+    // public GameObject battlePhasePlatform;
 
     public int startingScale;
 
@@ -23,7 +23,7 @@ public class RoundPhaseBattle : RoundPhase
     {
         // Setup current scale
         currentScale = startingScale;
-        battlePhasePlatform.transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+        // battlePhasePlatform.transform.localScale = new Vector3(currentScale, currentScale, currentScale);
 
         // Determine scaling rate
         float scaleDeltaPerPhase = (startingScale - endingScale) / GameManager.instance.numberOfRounds;
@@ -43,7 +43,7 @@ public class RoundPhaseBattle : RoundPhase
             if (roundTime - currentRoundTime >= timeUntilShrinkStarts && currentRoundTime > timeUntilPhaseEndAfterShrink)
             {
                 currentScale -= scaleDeltaPerSecond * Time.deltaTime;
-                battlePhasePlatform.transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+                // battlePhasePlatform.transform.localScale = new Vector3(currentScale, currentScale, currentScale);
             }
             if (currentRoundTime < 0)
             {
