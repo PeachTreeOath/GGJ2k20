a35d28ac2e19460bef1e6ec7799d6c61dbd9ab22
Calvin Pinsuwan <calvin@fizzpow.com>
2020-02-01 21:16:28 -0700

    Rockets

---
+++ Assets/Scripts/Bots/Rocket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : Projectile
+{
+    public override void Fire(Vector3 direction)
+    {
+        body.velocity = direction;
+    }
+
+    public void Explode()
+    {
+        Instantiate(ResourceLoader.instance.rocketExplosion);
+    }
+}
