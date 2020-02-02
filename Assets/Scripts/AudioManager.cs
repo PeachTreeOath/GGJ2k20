ace3bb45a2e55bb9e2976511ccc612873bc7b2a6
Unknown <rfonger@gmail.com>
2020-02-01 20:12:23 -0700

    Adding music to audio manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -32,8 +32,8 @@ public class AudioManager : Singleton<AudioManager>
         soundMap = new Dictionary<string, AudioClip>();
         soundMixer = new Dictionary<string, float>
         {
-            { "mimic_murder", .7f  },
-            { "wood", .4f },
+            { "RepairRoyale-intro", .5f  },
+            { "RepairRoyale-loop", .5f },
             { "Game_Over", .7f  },
             { "Gold_Escape", .7f  },
             { "mimic_game_over", .7f },
@@ -77,9 +77,9 @@ public class AudioManager : Singleton<AudioManager>
 
         // Retrieve the name of this scene.
         string sceneName = currentScene.name;
-        if (sceneName == "TitleScreen")
+        if (sceneName == "Game")
         {
-            PlayMusic("title_screen");
+            PlayMusicWithIntro("RepairRoyale-intro", "RepairRoyale-loop");
         }
         else if (sceneName == "LoadingScene")
         {
