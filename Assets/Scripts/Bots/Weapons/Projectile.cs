7ef4aa37a32c2970a15bc4cd1f46953022804de2
sketchycode <jeremiah.nunn@gmail.com>
2020-02-02 10:35:04 -0700

    fixes to projectiles to not collide with origin bot

--- Assets/Scripts/Bots/Weapons/Projectile.cs
+++ Assets/Scripts/Bots/Weapons/Projectile.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 
 public abstract class Projectile : MonoBehaviour
 {
-
+    public float Damage { get; set; }
     protected Rigidbody body;
+    public Transform IgnoreTransform { get; set;  }
 
     // Start is called before the first frame update
     protected virtual void Awake()
