18d4970a0dad4de77b1949e85e7a5b9ab0d3f45d
GregHilston <Gregory.Hilston@gmail.com>
2020-02-01 20:45:57 -0700

    merged something ayy

--- Assets/Scripts/Bots/Weapons/WeaponFlamethrower.cs
+++ Assets/Scripts/Bots/Weapons/WeaponFlamethrower.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class WeaponFlamethrower : Weapon
 {
-    // Start is called before the first frame update
-    void Start()
+	private void Awake()
+	{
+		TypeOfWeapon = WeaponType.Flamethrower;
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/ExampleBullet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleBullet : Projectile
+{
+    public override void Fire(Vector3 direction)
+    {
+        body.velocity = direction;
+    }
+}
